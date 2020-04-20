36559a4019f676efee0c75c3dcaf3ba94e8b393f
hangil-lee <62516413+hangil-lee@users.noreply.github.com>
2020-04-20 18:28:35 +0900

    Update 2 on GitHub branch Only

--- main.py
+++ main.py
@@ -20,3 +20,6 @@ print("Hello World Updates on GitHub branch")
 
 #Updates in local master branch
 print("Hello World Updates on Local Master Branch ")
+
+#Updates on mainbranch of GitHub
+print("Hello World Updates 2 on GitHub Only")
